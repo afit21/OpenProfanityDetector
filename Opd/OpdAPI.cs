105a02e5ea4855c5e9e279838e3c0c7ac1bc2f6a
afi <toetuuafi@gmail.com>
2022-04-04 16:27:11 +1000

    Project created

---
+++ Opd/OpdAPI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opd
+{
+    public class OpdAPI
+    {
+        public bool ProfanityDetected(string _phrase)
+        {
+            
+            return false;
+        }
+    }
+}
---
+++ Opd/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opd
+{
+    class Program
+    {
+        public static OpdAPI api;
+        static void Main(string[] args)
+        {
+            //api.ProfanityDetected(args[0]);
+            api.ProfanityDetected(args[0]);
+        }
+    }
+}
